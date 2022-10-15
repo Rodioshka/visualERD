0ab3acf547415ff61551e42aca8ca01bfcb42930
Rodioshka <ZyMamarKiZ34552BeGgi>
2022-10-15 19:36:49 +0300

    work version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import networkx as nx
+from pyvis.network import Network
+import community as community_louvain
+
 
 def get_data() -> dict:
     with open(r'data/entity', 'r', encoding='utf-8') as file:
@@ -9,9 +11,18 @@ def get_data() -> dict:
     return data
 
 
+def get_html(edge_to_label: dict) -> None:
+    Edge = nx.from_edgelist(edge_to_label.keys())
+    communities = community_louvain.best_partition(Edge)
+    nx.set_node_attributes(Edge, communities, 'group')
+    nt = Network(width='2000px', height='1000px', bgcolor='#222222', font_color='white')
+    nt.from_nx(Edge)
+    nt.show(name=r'er.html')
+
+
 def main():
     data = get_data()
-    print(data)
+    get_html(edge_to_label=data)
 
 
 if __name__ == '__main__':
